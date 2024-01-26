9573bad4baa62ddbaaf1269f298372cb06845a21
stillIsaac <isaacjanica@gmail.com>
2024-01-25 20:07:04 -0500

    Start console

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+void cot(std::string word) {
+    std::cout << word;
+};
+
+void cote(std::string word) {
+    std::cout << word << std::endl;
+};
 
 int main() {
-    
+    cote("Welcome to Scrabble the game of words and creativity!");
+    bool control = false;
+    std::string comand;
+    while(!control) {
+
+        cote("please add a command");
+        cot("$ ");
+        std::cin>>comand;
+
+        if(comand == "salir") {
+        
+            cote("Gracias por jugar, vuelve pronto");
+            control = true;
+        }
+
+
+    }
+
 }

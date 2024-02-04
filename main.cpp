2c934ab59e52a205c77b87ebde02430142c00a2e
MariaPaulaL <mariap.lopezr816@gmail.com>
2024-02-03 20:20:18 -0500

    menú ayuda

--- main.cpp
+++ main.cpp
@@ -244,57 +244,83 @@ int main() {
 
             if(comands[1] == "ayuda") {
 
-                cote("esto lo esta poniendo mapu");
+                cote("Muestra una descripcion de la funcion del comando y la explicacion de sus [posibles salidas]");
             }
 
-            else if( comands[1] == "inicializar" ) {
+            else if( comands[1] == "inicializar dic" ) {
                  
-                 cote("mapu AaAaAaAa");
+                 cote("Inicializa el sistema a partir un archivo que contiene un diccionario de palabras aceptadas en inglés.");
+                 cote("[Diccionario ya inicializado] Indica que el diccionario ya se ha inicializado previamente");
+                 cote("[Archivo no existe] Indica que el archivo que contiene el diccionario no existe o no puede ser leído");
+                 cote("[Resultado exitoso] Indica que el diccionario se ha inicializado correctamente.";)
             }
 
             else if( comands[1] == "iniciar_inverso" ) {
                  
-                 cote("mapu");
+                 cote("Inicializa el sistema a partir del archivo del diccionario almacenando las palabras en sentido inverso");
+                 cote("[Diccionario ya inicializado] Indica que el diccionario ya se ha inicializado previamente");
+                 cote("[Archivo no existe] Indica que el archivo que contiene el diccionario no existe o no puede ser leído");
+                 cote("[Resultado exitoso] Indica que el diccionario se ha inicializado correctamente.";)
+            
             }
 
-            else if( comands[1] == "puntaje" ) {
+            else if( comands[1] == "puntaje palabra" ) {
                  
-                 cote("mapu");
+                 cote("Obtiene el puntaje de una palabra dada.");
+                 cote("[Palabra no existe] Informa que la palabra no existe en el diccionario");
+                 cote("[Letras invalidas] Informa que la palabra contiene simbolos invalidos");
+                 cote("[Resultado exitoso] Muestra el puntaje de la palabra");
             }
 
             else if( comands[1] == "salir" ) {
                  
-                 cote("mapu");
+                 cote("Termina la ejecucion de la aplicacion");
             }
 
-            else if( comands[1] == "iniciar_arbol" ) {
+            else if( comands[1] == "iniciar_arbol diccionario.txt" ) {
                  
-                 cote("mapu");
+                 cote("Inicializa el sistema a partir del archivo del diccionario almacenando las palabras en uno o mas arboles de letras");
+                 cote("[Arbol ya inicializado] Indica que el arbol del diccionario ya se ha inicializado");
+                 cote("[Archivo no existe] El archivo del diccionario no existe o no puede ser leído");
+                 cote("[Resultado exitoso] El arbol del diccionario se ha inicializado correctamente");
             }
 
             else if( comands[1] == "iniciar_arbol_inverso" ) {
-                 
-                 cote("mapu");
+
+                 cote("Inicializa el sistema a partir del archivo del diccionario almacenando las palabras en arboles de letras en sentido inverso");
+                 cote("[Arbol ya inicializado] Indica que el arbol del diccionario inverso ya se ha inicializado");
+                 cote("[Archivo no existe] El archivo del diccionario no existe o no puede ser leído");
+                 cote("[Resultado exitoso] El arbol del diccionario inverso se ha inicializado correctamente");
+            
             }
 
             else if( comands[1] == "palabras_por_prefijo" ) {
                  
-                 cote("mapu");
+                 cote("Busca todas las palabras que inician con un prefijo dado");
+                 cote("[Prefijo invalido] El prefijo no pudo encontrarse en el diccionario");
+                 cote("[Resultado exitoso] Muestra las palabras encontradas en el diccionario con ese prefijo");
+                 
             }
 
             else if( comands[1] == "palabras_por_sufijo" ) {
                  
-                 cote("mapu");
+                 cote("Busca todas las palabras que inician con un sufijo dado");
+                 cote("[Prefijo invalido] El sufijo no pudo encontrarse en el diccionario");
+                 cote("[Resultado exitoso] Muestra las palabras encontradas en el diccionario con ese sufijo");
+                 
             }
 
             else if( comands[1] == "grafo_de_palabras" ) {
                  
-                 cote("este es el grafo loco de palabras");
+                 cote("Construye un grafo de palabras, conectando palabras que difieren en una unica letra");
+                 cote("[Resultado exitoso] Informa que el grafo se ha construido correctamente");
             }
 
             else if( comands[1] == "posibles_palabras" ) {
                  
-                 cote("esto es una explicacion de posibles palabras (toca cambiarlo)");
+                 cote("Muestra todas las posibles palabras validas a construir con la letras dadas");
+                 cote("[Letras invalidas] La palabra contiene simbolos invalidos");
+                 cote("[Resultado exitoso] Muestra las posibles palabras para construir con las letras dadas");
             }
         
         }

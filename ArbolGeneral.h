bf9ddfdbc9d96faad0ebd7aecfb651f368db36ab
MariaPaulaL <mariap.lopezr816@gmail.com>
2024-04-05 22:56:14 -0500

    traduccion

--- ArbolGeneral.h
+++ ArbolGeneral.h
@@ -1,29 +1,27 @@
 #ifndef _ARBOL_GENERAL_H
 #define _ARBOL_GENERAL_H
-
 #include <iostream>
 #include "NodoGeneral.h"
 #include <algorithm>
 
 class ArbolGeneral {
 
-    
+   public:
+       NodoGeneral* raiz;
+       ArbolGeneral();
 
-    public:
-        NodoGeneral* root;
-        ArbolGeneral();
 
-        void insert(std::string word);
-        bool iniciarArbol(std::string archivo);
-        bool iniciarArbolInverso(std::string archivo);
-        std::list<std::string> obtenerPalabrasPorPrefijo(std::string prefix);
-        std::list<std::string> obtenerPalabrasPorSufijo(std::string suffix);
-        void get_words_from_node(NodoGeneral* node, std::string prefix, std::list<std::string>& words);
-        void get_suffix_words(NodoGeneral* node,std::string suffix, std::list<std::string>& words);
+       void insert(std::string palabra);
+       bool iniciarArbol(std::string archivo);
+       bool iniciarArbolInverso(std::string archivo);
+       std::list<std::string> obtenerPalabrasPorPrefijo(std::string prefijo);
+       std::list<std::string> obtenerPalabrasPorSufijo(std::string sufijo);
+       void obtener_palabras_nodo(NodoGeneral* nodo, std::string prefijo, std::list<std::string>& palabras);
+       void obtener_sufijo_palabras(NodoGeneral* nodo,std::string sufijo, std::list<std::string>& palabras);
+
 
 };
 
+
 //#include "ArbolGeneral.cxx"
-
-
 #endif
